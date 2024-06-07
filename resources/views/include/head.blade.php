7b16a4bb712bdb161c2350db52199be3d42f22c7
BastienU <bastien.u04@gmail.com>
2024-06-07 10:09:04 +0200

    Mise à jour readme + connexion première vue

---
+++ resources/views/include/head.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>M2L</title>
+    <link rel="stylesheet" href="{{asset('css/main.css')}}">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+</head>
---
+++ resources/views/test.blade.php
@@ -0,0 +1,23 @@
+@include('include.head')
+<body>
+    <div class="Admin">
+        <h2>inscription des membres à modifier ou supprimer</h2>
+    
+        <h1>Membres inscrits</h1>
+    
+        <ul>
+            <li>Nom : le nom</li>
+            <li>Prénom : le prénom</li>
+            <li>Age : 20 ans</li>
+            <li>Ville : votre ville</li>
+            <li>Mail : admin@net.net</li>   
+        </ul>
+    
+        <p>Votre photo de profil</p>
+        <img src="./assets/?" alt="Photo de profil">
+    
+        <button type="button">Modifier</button>
+        <button type="button">Supprimer</button>
+    </div>
+</body>
+</html>
